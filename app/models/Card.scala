958c6def8e303879890a06f5efcb77fd19371b52
Matthew Harris <matthew.harris@digital.hmrc.gov.uk>
2020-01-13 14:22:29 +0000

    Implemented card model and unit tests

--- app/models/Card.scala
+++ app/models/Card.scala
@@ -1,5 +1,21 @@
 package models
 
-class Card {
+import play.api.libs.json._
+import play.api.mvc.PathBindable
 
+case class Card(_id: String)
+
+object Card {
+
+  implicit val reads: Reads[Card] = (__ \ "_id").read[String].map(Card(_))
+  implicit val writes: OWrites[Card] = (__ \ "_id").write[String].contramap(_._id)
+
+  implicit val pathBindable: PathBindable[Card] = {
+    new PathBindable[Card] {
+      override def bind(key: String, value: String): Either[String, Card] =
+        if (value.matches("^[a-zA-Z0-9]{16}$")) Right(Card(value)) else Left("The card ID you have entered is invalid")
+
+      override def unbind(key: String, value: Card): String = value._id
+    }
+  }
 }
--- test/models/CardSpec.scala
+++ test/models/CardSpec.scala
@@ -1,5 +1,38 @@
 package models
 
-class CardSpec {
+import models.Card.pathBindable
+import org.scalatest.{MustMatchers, OptionValues, WordSpec}
+import play.api.libs.json.Json
 
+class CardSpec extends WordSpec with OptionValues with MustMatchers {
+
+  val validCard = "As2Mn3Jh4Lk1JHj2"
+
+  "Card model" must {
+
+    "Deserialise" in {
+      val cardId = Card(_id = validCard)
+      val expectedJson = Json.obj("_id" -> validCard)
+
+      Json.toJson(cardId) mustEqual expectedJson
+    }
+
+    "Serialise" in {
+      val expectedCardId = Card(_id = validCard)
+      val json = Json.obj("_id" -> validCard)
+
+      json.as[Card] mustEqual expectedCardId
+    }
+
+    "return error if card id does not match regex pattern" in {
+      val invalidCard = "4yDUm7Nra7ALHuDr!"
+      val result = "The card ID you have entered is invalid"
+
+      pathBindable.bind("", invalidCard) mustBe Left(result)
+    }
+
+    "return a string" in {
+      pathBindable.unbind("", Card("testId0123456789")) mustEqual "testId0123456789"
+    }
+  }
 }
